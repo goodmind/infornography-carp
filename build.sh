fe7564ede4a734a53a73b8e50f6d460996e69668
andretshurotshka <andwebar@gmail.com>
2018-03-25 21:43:34 +0500

    Build tags from Travis

--- build.sh
+++ build.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
-carp -b infornography.carp
+
+TAG=$(git describe --tags --long | sed -E -e 's,^[^0-9]*,,;s,([^-]*-g),r\1,;s,[-_],.,g')
+CARP_DIR=$HOME/Carp
+
+clean () {
+  rm -rf *.tar.gz;
+  rm -rf out;
+  rm -rf dist;
+  mkdir dist;
+  mkdir out;
+}
+
+compress () {
+  mv out/Untitled dist/infornography;
+  tar -czf infornography-$TRAVIS_OS_NAME-$TAG.tar.gz -C dist infornography && echo 'Compressed';
+}
+
+build () {
+  clean;
+  carp -x infornography.carp && echo 'Building successful';
+  compress;
+}
+
+build;
